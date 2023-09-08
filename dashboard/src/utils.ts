702b325c98b2bc7d1844a98e17830e21cf164047
genevieveikechukwu <ikechukwummachukwu2020@gmail.com>
2023-09-08 14:51:02 +0100

    Pull from main

---
+++ dashboard/src/utils.ts
@@ -0,0 +1,97 @@
+interface DataType {
+    key: string,
+    productCode: string,
+    product: string,
+    instruction: string,
+    status: string,
+    date: string,
+    quantity: string
+
+}
+
+export const data: DataType[] = [
+  {
+    key: '2',
+    productCode: 'DEF456',
+    product: 'Product B',
+    instruction: 'Installation Needed',
+    status: 'Inactive',
+    date: '2023-02-20',
+    quantity: '200/300',
+  },
+  {
+    key: '3',
+    productCode: 'GHI789',
+    product: 'Product C',
+    instruction: 'User Manual Included',
+    status: 'Active',
+    date: '2023-03-10',
+    quantity: '800/800',
+  },
+  {
+    key: '4',
+    productCode: 'JKL012',
+    product: 'Product D',
+    instruction: 'Fragile Handling',
+    status: 'Active',
+    date: '2023-04-05',
+    quantity: '100/100',
+  },
+  {
+    key: '5',
+    productCode: 'MNO345',
+    product: 'Product E',
+    instruction: 'No Special Instructions',
+    status: 'Inactive',
+    date: '2023-05-12',
+    quantity: '350/500',
+  },
+  {
+    key: '6',
+    productCode: 'PQR678',
+    product: 'Product F',
+    instruction: 'Minimal Assembly',
+    status: 'Active',
+    date: '2023-06-18',
+    quantity: '300/400',
+  },
+  {
+    key: '7',
+    productCode: 'STU901',
+    product: 'Product G',
+    instruction: 'Professional Installation Required',
+    status: 'Inactive',
+    date: '2023-07-22',
+    quantity: '50/50',
+  },
+  {
+    key: '8',
+    productCode: 'VWX234',
+    product: 'Product H',
+    instruction: 'DIY Assembly',
+    status: 'Active',
+    date: '2023-08-30',
+    quantity: '600/700',
+  },
+  {
+    key: '9',
+    productCode: 'YZA567',
+    product: 'Product I',
+    instruction: 'No Assembly Required',
+    status: 'Inactive',
+    date: '2023-09-14',
+    quantity: '750/800',
+  },
+  {
+    key: '10',
+    productCode: 'BCD890',
+    product: 'Product J',
+    instruction: 'Custom Installation',
+    status: 'Active',
+    date: '2023-10-02',
+    quantity: '400/500',
+  },
+
+];
+
+console.log(data);
---
+++ dashboard/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,tsx}",
+  './pages/**/*.{html,tsx}',
+  './components/**/*.{html,tsx}'
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
