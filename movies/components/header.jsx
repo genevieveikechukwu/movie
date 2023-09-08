7d0ab138bd6b05a1ed8cd038f2543dee27209292
genevieveikechukwu <ikechukwummachukwu2020@gmail.com>
2023-09-08 14:42:16 +0100

    FEAT: Build user dashboard with antd

--- movies/components/header.jsx
+++ movies/components/header.jsx
@@ -4,7 +4,7 @@ function Header() {
     return(
         <div className="header">
             <header className=" w-full h-20 bg-gray-700 text-center p-4 fixed top-0 left-0">
-                <h1 className=" text-gray-300 text-xl font-bold">Movie Recommendation System</h1>
+                <h1 className=" font-extrabold md:text-xl" style={ { color: "#FFFFF0" } }>Movie Recommendation System</h1>
             </header>
         </div>
     )
